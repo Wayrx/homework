ea8d65807438f5a9345a8a8f27640ba3dbe94ae4
Wayne Reyovich <wayneflame4555@gmail.com>
2024-03-21 00:26:27 +0300

    first

---
+++ code.js
@@ -0,0 +1,13 @@
+let character = [] 
+    agility = "57",
+    strength = "85",
+    intelligence = "76",
+    damage = "73",
+    hp = 589
+
+
+("Agility: " + agility.log);
+("Strehgth: " + agility.log);
+("Intelligence: " + agility.log);
+("Damage: " + agility.log);
+("Hp: " + agility.log);
